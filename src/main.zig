eeda19cae9b387fbe4e3127160a41db531cf179d
sea gin <cijin@seagin.me>
2025-07-29 12:56:39 +0700

    header works too

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,11 @@ pub fn activate(app: *c.GtkApplication, user_data: c.gpointer) callconv(.C) void
     const window = c.gtk_application_window_new(app);
     c.gtk_window_set_title(@as(*c.GtkWindow, @ptrCast(window)), "Handmade Hero");
     c.gtk_window_set_default_size(@as(*c.GtkWindow, @ptrCast(window)), WIDTH, HEIGHT);
+
+    const header = c.gtk_header_bar_new();
+    c.gtk_header_bar_set_title_widget(@as(*c.GtkHeaderBar, @ptrCast(header)), c.gtk_label_new("Handmade Hero"));
+    c.gtk_window_set_titlebar(@as(*c.GtkWindow, @ptrCast(window)), header);
+
     c.gtk_window_present(@as(*c.GtkWindow, @ptrCast(window)));
 }
 
