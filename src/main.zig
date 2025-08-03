fda4d0ec1c0f425ea37b9555182596868e816bd4
sea gin <cijin@seagin.me>
2025-08-04 06:55:39 +0700

    finally window is open and recieving messages

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,10 @@
 const std = @import("std");
 const c = @cImport({
     @cInclude("X11/Xlib.h");
-    @cInclude("X11/Xutil.h");
 });
 
 const Width = 600;
 const Height = 480;
-const WindowName: c_char = "Yellow";
-const IconName = "X Window";
 
 pub fn main() !u8 {
     // On a POSIX-conformant system, if the display_name is NULL, it defaults to the value of the DISPLAY environment variable.
@@ -16,50 +13,59 @@ pub fn main() !u8 {
 
         return 1;
     };
-    defer c.XCloseDisplay(display);
+    defer _ = c.XCloseDisplay(display);
 
     const screen = c.XDefaultScreen(display);
-    const display_width = c.XDisplayWidth(display, screen);
-    const display_height = c.XDisplayHeight(display, screen);
-
-    const x: u16 = 0;
-    const y: u16 = 0;
-    const f: c_int = 3;
-    const w = @divTrunc(display_width, f);
-    const h = @divTrunc(display_height, f);
-    const border_width: c_uint = 0;
 
     const window_parent = c.XRootWindow(display, screen);
     const window = c.XCreateSimpleWindow(
         display,
         window_parent,
-        x,
-        y,
-        @intCast(w),
-        @intCast(h),
-        border_width,
+        0,
+        0,
+        Width,
+        Height,
+        0,
         c.XBlackPixel(display, screen),
         c.XWhitePixel(display, screen),
     );
-    defer c.XDestroyWindow(display, window);
 
-    var window_name: c.XTextProperty = undefined;
-    var icon_name: c.XTextProperty = undefined;
-    if (c.XStringListToTextProperty(WindowName, 1, &window_name) == 0) {
-        std.debug.print("structure allocation failed for window name", .{});
-
+    var delete_atom: c.Atom = undefined;
+    delete_atom = c.XInternAtom(display, "WM_DELETE_WINDOW", 0);
+    const protocol_status = c.XSetWMProtocols(display, window, &delete_atom, 1);
+    if (protocol_status == 0) {
+        std.debug.print("failed to set wm_delete protocol", .{});
         return 1;
     }
 
-    if (c.XStringListToTextProperty(&IconName, 1, &icon_name) == 0) {
-        std.debug.print("structure allocation failed for window name", .{});
+    _ = c.XStoreName(display, window, "Handmade");
 
-        return 1;
-    }
+    // you will not get events without this
+    _ = c.XSelectInput(display, window, c.KeyPressMask);
 
     _ = c.XMapWindow(display, window);
 
-    while (true) {}
+    // window will not show up without sync
+    _ = c.XSync(display, 0);
+
+    var quit = false;
+    var event: c.XEvent = undefined;
+    while (!quit) {
+        while (c.XPending(display) > 0) {
+            _ = c.XNextEvent(display, &event);
+            switch (event.type) {
+                c.KeyPress => std.debug.print("Key pressed, not sure which one.\n", .{}),
+                c.ClientMessage => {
+                    if (event.xclient.data.l[0] == delete_atom) {
+                        std.debug.print("Closing window.\n", .{});
+                        quit = true;
+                        break;
+                    }
+                },
+                else => continue,
+            }
+        }
+    }
 
     return 0;
 }
